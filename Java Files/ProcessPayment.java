681e61cba062fd9dca476a523ee285d75f5a9fa9
Pavan Vittala <avittala@gmu.edu>
2016-04-05 14:43:14 -0400

    Made changes to ProcessPayment

--- Java Files/ProcessPayment.java
+++ Java Files/ProcessPayment.java
@@ -7,6 +7,9 @@
  * 
  * This is the where all the payments are being processed through the use of functions.
  */
-public class ProcessPayment {
-
+public final class ProcessPayment {
+	public boolean processPayment(CreditCard card, int amount) {
+		System.out.println("Your card was verified");
+		return true;
+	}
 }
