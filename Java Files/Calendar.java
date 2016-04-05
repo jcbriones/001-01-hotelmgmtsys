dad735c52d5b436caf80be8b751d38ad7b9ad550
Matthew Edwards <medwar19@masonlive.gmu.edu>
2016-04-05 13:55:50 -0400

    Reservation and Calendar

--- Java Files/Calendar.java
+++ Java Files/Calendar.java
@@ -7,6 +7,18 @@
  * 
  * This is the Calendar class which preview the list of reservations in a calendar format.
  */
+import java.util.*;
+
 public class Calendar {
+    public Date Today;
+    public List TodayEvents;
+
+    public Date Today(){return Today;}
+
+    //Goes through the events  in the Hotel System
+    //And return all events that are happening at the provided date
+    public List EventsByDate(Date date) {
+
+    }
 
 }
--- Java Files/Reservation.java
+++ Java Files/Reservation.java
@@ -7,6 +7,89 @@
  * 
  * This is the Reservation class where good things happen. Just wait...
  */
-public class Reservation {
+public class Reservation
+{
+    private int ReservedToCustomerID;
+    private int RoomNumber;
+    private int NumberOfOccupants;
+    private Date ArrivalDate;
+    private Date DepartureDate;
+    private int Balance;
+    private int RoomCost;
 
+    public Reservation(int ReservedToCustomerID, int RoomNumber,
+                       int NumberOfOccupants, Date ArrivalDate,
+                       Date DepartureDate, int Balance, int RoomCost)
+    {
+        this.ReservedToCustomer = ReservedToCustomer;
+        this.RoomNumber = RoomNumber;
+        this.NumberOfOccupants = NumberOfOccupants;
+        this.ArrivalDate = ArrivalDate;
+        this.DepartureDate = DepartureDate;
+        this.Balance = Balance;
+        this.RoomCost = RoomCost;
+    }
+
+    public int getReservedToCustomerID() {
+        return ReservedToCustomerID;
+    }
+
+    public int getRoomNumber() {
+        return RoomNumber;
+    }
+
+    public int getNumberOfOccupants() {
+        return NumberOfOccupants;
+    }
+
+    public Date getArrivalDate() {
+        return ArrivalDate;
+    }
+
+    public Date getDepartureDate() {
+        return DepartureDate;
+    }
+
+    public int getBalance() {
+        return Balance;
+    }
+
+    public int getRoomCost() {
+        return RoomCost;
+    }
+
+    public void setReservedToCustomerID(int reservedToCustomerID) {
+        ReservedToCustomerID = reservedToCustomerID;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        RoomNumber = roomNumber;
+    }
+
+    public void setNumberOfOccupants(int numberOfOccupants) {
+        NumberOfOccupants = numberOfOccupants;
+    }
+
+    public void setArrivalDate(Date arrivalDate) {
+        ArrivalDate = arrivalDate;
+    }
+
+    public void setDepartureDate(Date departureDate) {
+        DepartureDate = departureDate;
+    }
+
+    public void setBalance(int balance) {
+        Balance = balance;
+    }
+
+    public void setRoomCost(int roomCost) {
+        RoomCost = roomCost;
+    }
+
+    //Should call ProcessPayment's validate Payment and send it
+    //This reservation's CreditCard
+    public boolean validatePayment()
+    {
+        return true;
+    }
 }
