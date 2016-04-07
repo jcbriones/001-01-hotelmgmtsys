b85a58fe9a4b37d723d8de1cc2847db452f738c0
Pavan Vittala <avittala@gmu.edu>
2016-04-07 14:28:34 -0400

    Almost finished createReport method

--- Java Files/Report.java
+++ Java Files/Report.java
@@ -9,16 +9,17 @@
  * This is the Report class which generate reports of the hotel system.
  */
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Report {
-	private int numebrOfReservations;
+	private int numberOfReservations;
 	private int numberOfSinglesReserved;
 	private int numberOfDoublesReserved;
 	private int occupancyRate;
-	private int totalRevenue;
+	private double totalRevenue;
 	
-	public Report(int reservations, int singles, int doubles, int occupancy, int revenue) {
-		this.numebrOfReservations = reservations;
+	public Report(int reservations, int singles, int doubles, int occupancy, double revenue) {
+		this.numberOfReservations = reservations;
 		this.numberOfSinglesReserved = singles;
 		this.numberOfDoublesReserved = doubles;
 		this.occupancyRate = occupancy;
@@ -27,7 +28,23 @@ public class Report {
 	
 	public Report createReport(Date first, Date second) {
 		Calendar cal = new Calendar();
-		double revenue = cal.RevinueByPeriod(first, second);
+		double revenue = cal.RevenueRange(first, second);
 		int occupancy = cal.TotalOcucpancy();
+		int numReservations = HotelSystem.getDB().getListOfReservations().size();
+		ArrayList<Room> list = HotelSystem.getDB().getListOfRooms();
+		Iterator<Room> iter = list.iterator();
+		int numDoubles = 0;
+		int numSingles = 0;
+		Room room;
+		while (iter.hasNext()) {
+			room = iter.next();
+			if (room.isDouble()) {
+				numDoubles++;
+			} else {
+				numSingles++;
+			}
+		}
+		occupancy = (occupancy/10)*100;
+		Report report = new Report(numReservations, numSingles, numDoubles, occupancy, revenue);
 	}
 }
