abcb185bafaf8908073968ad78a9d17c36be8032
jcbriones <jbrione3@gmu.edu>
2016-04-08 19:37:43 -0400

    Fixed Report.java

--- Java Files/Report.java
+++ Java Files/Report.java
@@ -9,41 +9,88 @@
  * This is the Report class which generate reports of the hotel system.
  */
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Report {
 	private int numberOfReservations;
 	private int numberOfSinglesReserved;
 	private int numberOfDoublesReserved;
-	private int occupancyRate;
+	private double occupancyRate;
 	private double totalRevenue;
-	
-	public Report(int reservations, int singles, int doubles, int occupancy, double revenue) {
+
+	public Report(int reservations, int singles, int doubles, int occupancy, double revenue)
+	{
 		this.numberOfReservations = reservations;
 		this.numberOfSinglesReserved = singles;
 		this.numberOfDoublesReserved = doubles;
 		this.occupancyRate = occupancy;
 		this.totalRevenue = revenue;
 	}
-	
-	public void generateReport(Date first, Date second) {
-		Calendar cal = new Calendar();
-		double revenue = cal.RevenueRange(first, second);
-		int occupancy = cal.TotalOcucpancy();
-		int numReservations = HotelSystem.getDB().getListOfReservations().size();
-		ArrayList<Room> list = HotelSystem.getDB().getListOfRooms();
-		Iterator<Room> iter = list.iterator();
-		int numDoubles = 0;
-		int numSingles = 0;
-		Room room;
-		while (iter.hasNext()) {
-			room = iter.next();
-			if (room.isDouble()) {
-				numDoubles++;
-			} else {
-				numSingles++;
-			}
+
+	public void generateReportRange(Date first, Date second)
+	{
+		// Reservation Lists on the given two dates
+		ArrayList<Reservation> rsvps = new Calendar().ReservationByDateRange(first, second);
+
+		for (int i = 0; i < rsvps.size(); i++)
+		{
+			// Number of Reservations
+			numberOfReservations++;
+
+			// Single Or Double
+			if (rsvps.get(i).getRoom().isDouble())
+				numberOfDoublesReserved++;
+			else
+				numberOfSinglesReserved++;
 		}
-		occupancy = (occupancy/10)*100;
 	}
+
+	public void generateReportAll()
+	{
+		ArrayList<Reservation> rsvps = HotelSystem.getDB().getListOfReservations();
+		for (int i = 0; i < rsvps.size(); i++)
+		{
+			// Number of Reservations
+			numberOfReservations++;
+
+			// Single Or Double
+			if (rsvps.get(i).getRoom().isDouble())
+				numberOfDoublesReserved++;
+			else
+				numberOfSinglesReserved++;
+		}
+
+		int counter = 0;
+		ArrayList<Room> rms = HotelSystem.getDB().getListOfRooms();
+		for (int i = 0; i < rms.size(); i++)
+			if (rms.get(i).isOccupied())
+				counter++;
+
+		occupancyRate = (counter / rms.size())*100;
+	}
+
+	/* =======================================
+	 * Setters and Getters
+	 * =======================================
+	 */
+
+	public int getNumberOfReservations() {
+		return numberOfReservations;
+	}
+
+	public int getNumberOfSinglesReserved() {
+		return numberOfSinglesReserved;
+	}
+
+	public int getNumberOfDoublesReserved() {
+		return numberOfDoublesReserved;
+	}
+
+	public double getOccupancyRate() {
+		return occupancyRate;
+	}
+
+	public double getTotalRevenue() {
+		return totalRevenue;
+	}
+
 }
