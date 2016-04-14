e972d8d334162f2a82917159dbd14e144333998b
Jc Briones <jbrione3@gmu.edu>
2016-04-13 20:39:36 -0400

    Fixed descriptions for Calendar class

--- Java Files/Calendar.java
+++ Java Files/Calendar.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Calendar.java
  * 
  * CS321-001 - George Mason University
@@ -6,27 +6,37 @@
  * @author: Jc Briones, Pavan Vittala, Ken Matsuda, Matt Edwards
  * 
  * This is the Calendar class which preview the list of reservations in a calendar format.
+ * Class that keeps track of Dates and what days the Hotel Rooms are free
  */
 import java.util.ArrayList;
 
-//Class that keeps track of Dates and what days the Hotel Rooms are free
 public class Calendar {
-	//this will hold all the reservations in our system.
+	/**
+	 * This will just point to the reservations inside the database.
+	 */
 	private ArrayList<Reservation> listOfReservations;
 
-	// Constructor
+	/**
+	 * Constructor
+	 * @param hs
+	 */
 	public Calendar(HotelSystem hs)
 	{
 		listOfReservations = hs.getDB().getListOfReservations();
 	}
 
-	//given a room and a list of dates, checks to see if the room is available
-	//Author: Matt Edwards
+	/**
+	 * Description: Given a room and a list of dates, checks to see if the room is available.
+	 * @param rm
+	 * @param dates
+	 * @return boolean
+	 * @author Matt Edwards
+	 */
 	public boolean checkDate(Room rm, ArrayList<Date> dates)
 	{
-		//goes through the list of reservations
-		//then checks that reservation's date range
-		//then checks to see if the given dates are equal to any of the dates in the date range
+		// Goes through the list of reservations
+		// Then checks that reservation's date range
+		// Then checks to see if the given dates are equal to any of the dates in the date range
 		for (int i = 0; i < listOfReservations.size(); i++)
 			for (int j = 0; j < listOfReservations.get(i).getDates().size(); j++)
 				for (int k = 0; k < dates.size(); k++)
@@ -35,8 +45,13 @@ public class Calendar {
 		return true;
 	}
 
-	// Given a range of dates, collect the list of Reservation during that given time.
-	//Author: Matt Edwards
+	/**
+	 *  Given a range of dates, collect the list of Reservation during that given time.
+	 * @param day1
+	 * @param day2
+	 * @return ArrayList<Reservation>
+	 * @author Matt Edwards
+	 */
 	public ArrayList<Reservation> ReservationByDateRange(Date day1, Date day2)
 	{
 		// Error checking for if day2 is before day1
