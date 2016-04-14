941a25769e8ee108a3bfbc49240ed30af95d5280
Pavan Vittala <avittala@gmu.edu>
2016-04-13 20:19:50 -0400

    Fix descriptions under Database

--- Java Files/Database.java
+++ Java Files/Database.java
@@ -1,24 +1,30 @@
-/*
+/**
  * Database.java
  * 
  * CS321-001 - George Mason University
  * Group 01
  * @author: Jc Briones, Pavan Vittala, Ken Matsuda, Matt Edwards
  * 
- * This is the HotelSystem's database where all the data is stored.
+ * This class acts as a storage class for all of the information in the HotelSystem.
+ * This class holds 3 lists: listOfRooms, listOfReservations, and a listOfUsers.
+ * This class updates and maintains new information about the HotelSystem as customers arrive, book-in, check-out, and leave.
  */
 import java.util.ArrayList;
 
-//This class acts as a storage class for all of the information in the HotelSystem.
-//This class holds 3 lists:
-//listOfRooms, listOfReservations, and a listOfUsers.
-//This class updates and maintains new information about the HotelSystem as customers arrive, book-in, check-out, and leave.
+
+
 public class Database {
+	/**
+	 * Class Variables
+	 */
 	private ArrayList<Room> listOfRooms;
 	private ArrayList<Reservation> listOfReservations;
 	private ArrayList<User> listOfUsers;
 
-	// Constructor
+	/**
+	 * Description: This is where an instance of a single database is created which will hold all the data.
+	 * @author Matt Edwards
+	 */
 	public Database()
 	{
 		listOfRooms = new ArrayList<Room>();
